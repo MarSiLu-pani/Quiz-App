a0b2bc49c172501e1e1ec10d5bfe3a1727aa67a4
Naasgul <lnaas@hotmail.de>
2020-10-24 15:25:46 +0200

    Add functionality to distinguish between correct and incorrect answers in .map-function

--- src/components/AnswerContainer.js
+++ src/components/AnswerContainer.js
@@ -8,41 +8,16 @@ function AnswerContainer(answers, correct_answer) {
       innerHTML: element,
     })
   );
+  let correctAnswerButton = createElement("button", {
+    className: "quizcard__answer__buttons",
+    innerHTML: correct_answer,
+  });
 
   const answerContainer = createElement("div", {
     className: "quizCard__answers",
-    children: [...incorrectAnswerButtons],
+    children: [...incorrectAnswerButtons, correctAnswerButton],
   });
   return answerContainer;
 }
 
 export default AnswerContainer;
-
-/* function AnswerContainer(answers, correct_answer) {
-  const answer1 = answers[0];
-  const answerButton1 = createElement("button", {
-    className: "quizCard__answers__button",
-    innerHTML: answer1,
-  });
-  const answer2 = answers[1];
-  const answerButton2 = createElement("button", {
-    className: "quizCard__answers__button",
-    innerHTML: answer2,
-  });
-  const answer3 = answers[2];
-  const answerButton3 = createElement("button", {
-    className: "quizCard__answers__button",
-    innerHTML: answer3,
-  });
-  const answer4 = answers[3];
-  const answerButton4 = createElement("button", {
-    className: "quizCard__answers__button",
-    innerHTML: answer4,
-  });
-
-  const answerContainer = createElement("div", {
-    className: "quizCard__answers",
-    children: [answerButton1, answerButton2, answerButton3, answerButton4],
-  });
-  return answerContainer;
-} */
