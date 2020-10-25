e51b365cdda2ff5f64e0d8582494fafb1c1ab9b8
Naasgul <lnaas@hotmail.de>
2020-10-25 14:36:44 +0100

    :bumm: Add randomizer for answers

--- src/components/AnswerContainer.js
+++ src/components/AnswerContainer.js
@@ -2,20 +2,27 @@ import { createElement } from "../utils/elements";
 import "./quizCard.css";
 
 function AnswerContainer(answers, correct_answer) {
-  let incorrectAnswerButtons = answers.map((element) =>
+  const allAnswers = [...answers];
+
+  allAnswers.push(correct_answer);
+
+  function randomizeAnswers(arr) {
+    arr.sort(() => Math.random() - 0.5);
+  }
+
+  randomizeAnswers(allAnswers);
+  console.log(allAnswers);
+
+  let allAnswersRandom = allAnswers.map((element) =>
     createElement("button", {
       className: "quizcard__answer__buttons",
       innerHTML: element,
     })
   );
-  let correctAnswerButton = createElement("button", {
-    className: "quizcard__answer__buttons",
-    innerHTML: correct_answer,
-  });
 
   const answerContainer = createElement("div", {
     className: "quizCard__answers",
-    children: [...incorrectAnswerButtons, correctAnswerButton],
+    children: [...allAnswersRandom],
   });
   return answerContainer;
 }
