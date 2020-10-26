539a477f0e06d78642f3ede354cac2ce93e821ed
Sina-Sche <si.scheckenbach@gmail.com>
2020-10-26 08:46:38 +0100

    💩Add change color - but not working properly

--- src/components/AnswerContainer.js
+++ src/components/AnswerContainer.js
@@ -13,10 +13,30 @@ function AnswerContainer(answers, correct_answer) {
   randomizeAnswers(allAnswers);
   console.log(allAnswers);
 
+  // function changeCorrectColor() {
+  //   const button = document.querySelector(".quizcard__answer__buttons");
+  //   button.classList.toggle("correct__answer__button");
+  // }
+
+  // function changeIncorrectColor() {
+  //   const button = document.querySelector(".quizcard__answer__buttons");
+  //   button.classList.toggle("incorrect__answer__buttons");
+  // }
+
   let allAnswersRandom = allAnswers.map((element) =>
     createElement("button", {
       className: "quizcard__answer__buttons",
       innerHTML: element,
+      onclick: function () {
+        if (element === correct_answer) {
+          const button = document.querySelector(".quizcard__answer__buttons");
+          button.classList.add("correct__answer__button");
+        }
+        //  else {
+        //   const button = document.querySelector(".quizcard__answer__buttons");
+        //   button.classList.add("incorrect__answer__buttons");
+        // }
+      },
     })
   );
 
